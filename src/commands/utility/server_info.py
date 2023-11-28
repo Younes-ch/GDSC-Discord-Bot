9020ea0c2bc6b287c92803ccdb106f2144fec319
Younes-ch <younes.jmell10@gmail.com>
2023-11-28 23:14:15 +0100

    Redesigned serverinfo & userinfo embed.

--- src/commands/utility/server_info.py
+++ src/commands/utility/server_info.py
@@ -8,33 +8,33 @@ class ServerInfo(commands.Cog):
 
     @app_commands.command(name='serverinfo', description='Display information about the server.')
     async def server_info(self, interaction: discord.Interaction):
+        users = [user for user in interaction.guild.members if not user.bot]
         statuses_count = {
-            'online': sum(1 for m in interaction.guild.members if m.status.name == 'online'),
-            'idle': sum(1 for m in interaction.guild.members if m.status.name == 'idle'),
-            'dnd': sum(1 for m in interaction.guild.members if m.status.name == 'dnd'),
-            'offline': sum(1 for m in interaction.guild.members if m.status.name == 'offline')
+            'online': sum(1 for m in users if m.status == discord.Status.online),
+            'idle': sum(1 for m in users if m.status == discord.Status.idle),
+            'dnd': sum(1 for m in users if m.status == discord.Status.dnd)
         }
         guild_name =interaction.guild.name
         guild_text_channels = len(interaction.guild.text_channels)
         guild_voice_channels = len(interaction.guild.voice_channels)
         guild_categories = len(interaction.guild.categories)
         guild_member_count = interaction.guild.member_count
-        guild_human_members = len([x for x in interaction.guild.members if not x.bot])
-        guild_bot_members = guild_member_count - guild_human_members
+        guild_users_members = len(users)
+        guild_bot_members = guild_member_count - guild_users_members
         guild_banned_members = len([entry async for entry in interaction.guild.bans(limit=2000)])
-        guild_member_statuses = f'🟢 {statuses_count["online"]} | 🟠 {statuses_count["idle"]} | ⛔ {statuses_count["dnd"]} | ⚪ {statuses_count["offline"]}'
+        guild_user_status_count = f'🟢 {statuses_count["online"]} | ⛔ {statuses_count["dnd"]} | 🌙 {statuses_count["idle"]}'
         guild_roles_count = len(interaction.guild.roles)
         guild_highest_role = interaction.guild.roles[-1].mention
         guild_description = interaction.guild.description if interaction.guild.description else 'N/A'
         footer_text ='Guild ID: {} • Created at: {}'.format(interaction.guild.id, interaction.guild.created_at.strftime("%d-%b-%Y"))
         embed = discord.Embed(title='Server information', color=interaction.user.top_role.color)
         embed.add_field(name='Name:', value=guild_name)
-        embed.add_field(name='Avatar:', value="[Click Here]({})".format(interaction.guild.icon.url))
+        embed.add_field(name='Avatar:', value="[Link]({})".format(interaction.guild.icon.url))
         embed.add_field(name='Members:', value=guild_member_count)
-        embed.add_field(name='Humans:', value=guild_human_members)
+        embed.add_field(name='Users:', value=guild_users_members)
         embed.add_field(name='Bots:', value=guild_bot_members)
         embed.add_field(name='Banned:', value=guild_banned_members)
-        embed.add_field(name='Statuses:', value=guild_member_statuses)
+        embed.add_field(name='Status count:', value=guild_user_status_count)
         embed.add_field(name='Text channels:', value=guild_text_channels)
         embed.add_field(name='Voice channels:', value=guild_voice_channels)
         embed.add_field(name='Categories:', value=guild_categories)
@@ -44,7 +44,7 @@ class ServerInfo(commands.Cog):
         embed.add_field(name='Description:', value=guild_description, inline=False)
         embed.set_thumbnail(url=interaction.guild.icon.url)
         embed.set_footer(text=footer_text)
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.response.send_message(embed=embed)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(ServerInfo(bot))
--- src/commands/utility/user_info.py
+++ src/commands/utility/user_info.py
@@ -24,19 +24,23 @@ class UserInfo(commands.Cog):
         }
         guild = interaction.guild
         status = statuses[guild.get_member(member.id).status.name]
-        embed = discord.Embed(title='User information:', color = member.top_role.color)
-        embed.add_field(name='Name', value=member)
+        try:
+            color = member.top_role.color if member.top_role.color != discord.Color.default() else member.roles[-2].color
+        except IndexError:
+            color = discord.Color.default()
+        embed = discord.Embed(title=member.display_name, color = color)
         embed.add_field(name='ID', value=member.id)
-        embed.add_field(name='Status', value=status)
+        embed.add_field(name='Avatar:', value="[Link]({})".format(member.display_avatar.url))
+        embed.add_field(name='Activity', value=f'{str(member.activity.type).split(".")[-1].title()} **{str(member.activity.name)}**!' if member.activity else 'N/A')
         embed.add_field(name='Bot?', value='✅' if member.bot else '❌')
         embed.add_field(name='Booster', value=member.premium_since.strftime("%d-%b-%Y") if member.premium_since else '❌')
-        embed.add_field(name='Activity', value=f'{str(member.activity.type).split(".")[-1].title()} **{str(member.activity.name)}**!' if member.activity else 'N/A')
+        embed.add_field(name='Status', value=status)
         embed.add_field(name='Created at', value=member.created_at.strftime("%d-%b-%Y"))
         embed.add_field(name='Joined at', value=member.joined_at.strftime("%d-%b-%Y"))
+        embed.add_field(name='Top Role', value=member.top_role.mention)
         embed.set_thumbnail(url=member.display_avatar.url)
-        embed.add_field(name='Roles', value="\n".join([x.mention for x in member.roles if x.name != '@everyone']) if len(member.roles) > 1 else 'N/A')
         embed.set_footer(text='Requested by {}'.format(interaction.user.name), icon_url = interaction.user.display_avatar.url)
-        await interaction.response.send_message(embed=embed, ephemeral=True)
+        await interaction.response.send_message(embed=embed)
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(UserInfo(bot))
